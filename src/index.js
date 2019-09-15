faa5592233fda01aa7717df156cd9c36da65c0ac
Tim Austin <tim@neenjaw.com>
2019-09-15 08:23:43 -0600

    tutorial start

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Game } from "./r/game";
+
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic hapi-haps!</h2>
+      <h1>React Tutorial - Tic Tac Toe</h1>
+      <h2>A follow-along react adventure!</h2>
+
+      <Game />
     </div>
   );
 }
