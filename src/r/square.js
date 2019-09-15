18e22c06aa1d077635d20bd5e997ddcb738fc653
Tim Austin <tim@neenjaw.com>
2019-09-15 09:00:22 -0600

    board passes props

--- src/r/square.js
+++ src/r/square.js
@@ -2,6 +2,10 @@ import React from "react";
 
 export class Square extends React.Component {
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
   }
 }
